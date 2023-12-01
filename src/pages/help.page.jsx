a002cd38cdc79d18bd11dc1a8f68cee2ab609349
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-01 15:12:13 -0500

    Add Help page and remove Home component

---
+++ src/pages/help.page.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+const Help = () => {
+  return (
+    <main>
+      <h1>Need Help? You&apos;re at the right spot!</h1>
+      <Outlet />
+    </main>
+  )
+}
+
+export default Help
