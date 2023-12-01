3de58ec3d3f88e7795508bf8bce56cddc5863b5a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-01 14:55:37 -0500

    Added comment to help explain first parent Route

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import App from './App.jsx'
 import Home from './pages/Home.component.jsx'
 import About from './pages/about.component.jsx'
 
+// first parent route is the layout route, the use outlet in whatever component you used
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
