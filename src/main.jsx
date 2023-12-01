c9df23f3579d0cc86a9e7349f2c8c9a33a5c2507
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-01 16:39:53 -0500

    Add Careers page and layout and inline loader function

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,8 @@ import Help from './pages/help.page.jsx'
 import FAQ from './pages/help/faq.page.jsx'
 import Contact from './pages/help/contact.page.jsx'
 import NotFound from './pages/not-found.page.jsx'
+import CareersLayout from './layouts/careers-layout.page.jsx'
+import Careers from './pages/careers/careers.page.jsx'
 
 // first parent route is the layout route, the use outlet in whatever component you used
 const router = createBrowserRouter(
@@ -29,11 +31,24 @@ const router = createBrowserRouter(
         <Route path='contact' element={<Contact />} />
       </Route>
 
+      <Route path='careers' element={<CareersLayout />}>
+        <Route
+          index
+          element={<Careers />}
+          loader={async () => {
+            const res = await fetch('http://localhost:3001/careers')
+            return await res.json()
+          }}
+        />
+      </Route>
+
       <Route path='*' element={<NotFound />} />
     </Route>
   )
 )
 // nested routes => will render help layout and the index/default element which is the FAQ component => /help/faq or /help/contact
+
+// added inline loader function but can be an exported function as well
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
