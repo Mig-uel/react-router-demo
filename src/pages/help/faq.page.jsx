6e75bca171fcbe101fc0ddaa9146e25d4e6a7528
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-01 15:53:41 -0500

    Add FAQ page

---
+++ src/pages/help/faq.page.jsx
@@ -0,0 +1,65 @@
+const faq = () => {
+  return (
+    <section className='faq'>
+      <h3>Frequently Asked Questions</h3>
+
+      <div className='question'>
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+          perferendis enim laborum quis aliquid sit! Blanditiis numquam cum
+          soluta cupiditate est doloremque dolor earum. Possimus quo deleniti
+          tempore pariatur iusto.
+        </p>
+      </div>
+      <div className='question'>
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+          perferendis enim laborum quis aliquid sit! Blanditiis numquam cum
+          soluta cupiditate est doloremque dolor earum. Possimus quo deleniti
+          tempore pariatur iusto.
+        </p>
+      </div>
+      <div className='question'>
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+          perferendis enim laborum quis aliquid sit! Blanditiis numquam cum
+          soluta cupiditate est doloremque dolor earum. Possimus quo deleniti
+          tempore pariatur iusto.
+        </p>
+      </div>
+      <div className='question'>
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+          perferendis enim laborum quis aliquid sit! Blanditiis numquam cum
+          soluta cupiditate est doloremque dolor earum. Possimus quo deleniti
+          tempore pariatur iusto.
+        </p>
+      </div>
+      <div className='question'>
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+          perferendis enim laborum quis aliquid sit! Blanditiis numquam cum
+          soluta cupiditate est doloremque dolor earum. Possimus quo deleniti
+          tempore pariatur iusto.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default faq
