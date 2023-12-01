b978e5eb5acc2c109187618aa7ceb4b2ea434847
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-01 16:39:59 -0500

    Add CareersLayout component

---
+++ src/layouts/careers-layout.page.jsx
@@ -0,0 +1,13 @@
+import { Outlet, NavLink } from 'react-router-dom'
+
+const CareersLayout = () => {
+  return (
+    <section>
+      <h3>Careers</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit?</p>
+      <Outlet />
+    </section>
+  )
+}
+
+export default CareersLayout
